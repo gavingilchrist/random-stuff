b3869217023a93a7884cf0a6fa4f3e679487854a
gavingilchrist <gavin.d.gilchrist@gmail.com>
2025-05-22 10:49:02 -0400

    Ch-ch-ch-changes

--- cython_learning/basic_test/.ipynb_checkpoints/cython_example-checkpoint.pyx
+++
@@ -1,6 +0,0 @@
-def sum_of_squares_cy(int n):
-    cdef long long result = 0
-    cdef long long i
-    for i in range(n):
-        result += i * i
-    return result
--- cython_learning/basic_test/pure_python_example.py
+++
@@ -1,5 +0,0 @@
-def sum_of_squares_py(n):
-    result = 0
-    for i in range(n):
-        result += i * i
-    return result
